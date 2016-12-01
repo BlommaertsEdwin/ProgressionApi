eed4e83050945e13b3919588e526db99a432eeec
Blommaerts Edwin <edwin.blommaerts@gmail.com>
2016-12-01 15:53:54 +0000

    Yes that was the straw...

--- flask_app.py
+++ flask_app.py
@@ -33,14 +33,14 @@ class Party(Resource):
 # Todo
 # shows a single todo item and lets you delete a todo item
 class Todo(Resource):
-        def get(self, todo_id):
-                abort_if_todo_doesnt_exist(todo_id)
-                return TODOS[todo_id]
+	def get(self, todo_id):
+		abort_if_todo_doesnt_exist(todo_id)
+		return TODOS[todo_id]
 
 def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
-        return '', 204
+	abort_if_todo_doesnt_exist(todo_id)
+	del TODOS[todo_id]
+	return '', 204
 
 def put(self, todo_id):
         args = parser.parse_args()
