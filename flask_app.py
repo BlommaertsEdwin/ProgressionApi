95464aa8701b74b2cb7b392db46b43b19418d2d1
Blommaerts Edwin <edwin.blommaerts@gmail.com>
2016-12-01 16:41:07 +0000

    hooolalala

--- flask_app.py
+++ flask_app.py
@@ -56,12 +56,11 @@ class TodoList(Resource):
                 return TODOS
         def post(self):
                 args = parser.parse_args()
-		todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-		todo_id = 'todo%i' % todo_id
-		TODOS[todo_id] = {'task': args['task']}
-		return TODOS[todo_id], 201
+                todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+                todo_id = 'todo%i' % todo_id
+                TODOS[todo_id] = {'task': args['task']}
+                return TODOS[todo_id], 201
 
-##
 ## Actually setup the Api resource routing here
 ##
 api.add_resource(TodoList, '/todos', endpoint='todos')
