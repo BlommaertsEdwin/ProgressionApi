dd2ea1fdfd80bbfae6f012a3568e2d6e9086f89d
Blommaerts Edwin <edwin.blommaerts@gmail.com>
2016-12-07 02:25:38 +0000

    Refactored the class method

--- flask_app.py
+++ flask_app.py
@@ -4,93 +4,54 @@ from flask_restful import reqparse, abort, Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-TODOS = {
-                'todo1': {'task': 'build an API'},
-                'todo2': {'task': '?????'},
-                'todo3': {'task': 'profit!'},
-                }
-
 PARTY = {}
 
 
-def abort_if_todo_doesnt_exist(todo_id):
-        if todo_id not in TODOS:
-                abort(404, message="Todo {} doesn't exist".format(todo_id))
+def abort_if_party_member_doesnt_exist(member_id):
+        if member_id not in PARTY:
+                abort(404, message="Todo {} doesn't exist".format(member_id))
 
-def abort_if_party_member_doesnt_exist(todo_id):
-        if todo_id not in TODOS:
-                abort(404, message="Todo {} doesn't exist".format(todo_id))
-
-def check_if_list_is_empty():
-        if not PARTY:
-                abort(404, message="No party members have been added, please add them using the 'add_party_member' method")
 
 parser = reqparse.RequestParser()
 parser.add_argument('task')
 
-class Party(Resource):
-        def get(self):
-                check_if_list_is_empty()
-                return "Ok"
-        def post(self):
-                args = parser.parse_args()
-                member_id = int(max(PARTY.keys()).lstrip('member')) + 1
-                todo_id = 'member%i' % member_id
-                PARTY[member_id] = {'name': args['name'], 'level': args['level']}
-                return PARTY[member_id], 201
-        
-class PartyMember(Resource):
-        def get(self, member_id):
-                abort_if_party_member_doesnt_exist(member_id)
-                return PARTY[member_id]
-        def delete(self, member_id):
-                abort_if_party_member_doesnt_exist(member_id)
-                del PARTY[member_id]
-                return '', 204
-        def put(self, member_id):
-                args = parser.parse_args()
-                PartyMember = {'name': args['name'], 'level': args['level']}
-                PARTY[member_id] = PartyMember
-                return task, 201
 
 # Todo
 # shows a single todo item and lets you delete a todo item
-class Todo(Resource):
-	def get(self, todo_id):
-		abort_if_todo_doesnt_exist(todo_id)
-		return TODOS[todo_id]
+class Member(Resource):
+	def get(self, member_id):
+		abort_if_party_member_doesnt_exist(member_id)
+		return PARTY[member_id]
 
-	def delete(self, todo_id):
-		abort_if_todo_doesnt_exist(todo_id)
-		del TODOS[todo_id]
+	def delete(self, member_id):
+		abort_if_party_member_doesnt_exist(member_id)
+		del PARTY[member_id]
 		return '', 204
 
-	def put(self, todo_id):
+	def put(self, member_id):
 		args = parser.parse_args()
-		task = {'task': args['task']}
-		TODOS[todo_id] = task
-		return task, 201
+		member = {'name': args['name'], 'level': args['level']}
+		PARTY[member_id] = member
+		return member, 201
 
 
 # TodoList
 # shows a list of all todos, and lets you POST to add new tasks
-class TodoList(Resource):
+class Party(Resource):
         def get(self):
-                return TODOS
+                return PARTY
         def post(self):
                 args = parser.parse_args()
-                todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-                todo_id = 'todo%i' % todo_id
-                TODOS[todo_id] = {'task': args['task']}
-                return TODOS[todo_id], 201
+                member_id = int(max(TODOS.keys()).lstrip('member')) + 1
+                member_id = 'member%i' % member_id
+                PARTY[member_id] = {'name': args['name'], 'level': args['level']}
+                return PARTY[member_id], 201
 
 ## Actually setup the Api resource routing here
 ##
-api.add_resource(TodoList, '/todos', endpoint='todos')
-api.add_resource(Todo, '/todos/<todo_id>', endpoint='todo')
-api.add_resource(Party, '/party', endpoint='party')
-# api.add_resource(PartyMember, '/partymember', endpoint='partymember')
-api.add_resource(PartyMember, '/partymember/<member_id>', endpoint='partymember')
+api.add_resource(Party, '/todos', endpoint='party')
+api.add_resource(Member, '/todos/<todo_id>', endpoint='member')
+
 
 @app.route('/')
 def home():
